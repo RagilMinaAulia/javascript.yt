012a18f1221097705529603bbc03e816f21d03a0
Ragil Mina Aulia <140393700+RagilMinaAulia@users.noreply.github.com>
2023-11-21 08:47:45 +0700

    versi 2.3
    Document Object Model

---
+++ js/dom.js
@@ -0,0 +1,4 @@
+document.querySelector("h1").innerText = "saya belajar javascript";
+document.querySelector("p").innerText = "belajar javascript itu mudah";
+document.querySelector(".isi").innerText = "saya belajar js di smk revit";
+document.querySelector("#judul").innerText = "belajar js";
